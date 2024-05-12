e69fa1a8cde92f3f8a3f3ea645a84d31e2db1d29
wetooa <derpykidyt@gmail.com>
2024-05-12 11:58:55 +0800

    initialized folder structure

--- app/src/main/java/com/example/begoussediccouse_finalproject/profile/Profile.java
+++ app/src/main/java/com/example/begoussediccouse_finalproject/profile/Profile.java
@@ -1,4 +1,4 @@
-package com.example.begoussediccouse_finalproject;
+package com.example.begoussediccouse_finalproject.profile;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.begoussediccouse_finalproject.R;
+
+public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
